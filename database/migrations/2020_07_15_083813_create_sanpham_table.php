df8f15c819dbb6004e6a076912d2dd0663f7cb69
Minh <minhmyn97@gmail.com>
2020-07-15 15:45:32 +0700

    Manage database with migrate

---
+++ database/migrations/2020_07_15_083813_create_sanpham_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSanphamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sanpham', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Ten', 255);
+            $table->integer('SoLuong')->default(0);
+            $table->float('Gia');
+            $table->integer('loaisanpham_id')->unsigned();
+            $table->foreign('loaisanpham_id')->references('id')->on('loaisanpham');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sanpham');
+    }
+}
